6c283d1783ca3d58b46a69036ef98853a29b9a3f
parasg97 <parasg1997@gmail.com>
2018-07-23 16:02:31 +0530

    interstitial add added

---
+++ app/src/main/java/com/persi/adapp/InterstitialActivity.java
@@ -0,0 +1,39 @@
+package com.persi.adapp;
+
+import android.app.NativeActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class InterstitialActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interstitial);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+    }
+
+    public void showAd(View v){
+        if(mInterstitialAd.isLoaded()){
+            mInterstitialAd.show();
+        }else{
+            Toast.makeText(this,"add not loaded",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void buttonPressed(View v){
+        Intent goToNativeAd=new Intent(this, com.persi.adapp.NativeActivity.class );
+        finish();
+        startActivity(goToNativeAd);
+    }
+}
--- app/src/main/java/com/persi/adapp/MainActivity.java
+++ app/src/main/java/com/persi/adapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.persi.adapp;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,71 +10,48 @@ import android.widget.TextView;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
+import java.util.Date;
+
+import static com.google.android.gms.ads.AdRequest.DEVICE_ID_EMULATOR;
+
+/*This class is created for banner ads.
+They stay on screen while users are interacting with the app,
+and can refresh automatically after a certain period of time. I
+ */
 public class MainActivity extends AppCompatActivity {
     private final String APP_ID="ca-app-pub-3940256099942544~3347511713";
     private AdView mAdView;
     private Button mButton;
-    private TextView mTextView;
     private TextInputEditText mTextInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MobileAds.initialize(this, APP_ID);
+        MobileAds.initialize(this, APP_ID);//initialized should be called only once when app is created
+        // TODO: 7/23/2018 add adsize and addunit id programmatically
         mAdView = findViewById(R.id.main_adView);
+        /*An AdRequest contains targeting information used to fetch an ad
+        * we can set information like gender,birthday,location etc. so that user
+        * can get more relevant ads*
+        * eg. adResquest.setGender()*/
         AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice("97F84E2DAA26F15D8141753F76ED0932")
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)//if our app is test app and we are not using the dummy app id provided in documentation we should mark our device as test device
+                //.setGender(AdRequest.GENDER_MALE)
                 .build();
-        if(adRequest.isTestDevice(this)){
-            mAdView.loadAd(adRequest);
-        }
-
+        mAdView.loadAd(adRequest);
         mButton=findViewById(R.id.main_button);
-        mTextView=findViewById(R.id.main_textview);
-        mTextInputEditText=findViewById(R.id.main_text_input);
-        mAdView.setAdListener(new AdListener(){
-            @Override
-            public void onAdClosed() {
-                super.onAdClosed();
-            }
 
-            @Override
-            public void onAdFailedToLoad(int i) {
-                super.onAdFailedToLoad(i);
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                super.onAdLeftApplication();
-            }
-
-            @Override
-            public void onAdOpened() {
-                super.onAdOpened();
-            }
-
-            @Override
-            public void onAdLoaded() {
-                super.onAdLoaded();
-            }
-
-            @Override
-            public void onAdClicked() {
-                super.onAdClicked();
-            }
-
-            @Override
-            public void onAdImpression() {
-                super.onAdImpression();
-            }
-        });
+        
     }
 
     public void buttonPressed(View v){
-        mTextView.setText(mTextInputEditText.getText().toString());
+        Intent goToInterstitial=new Intent(this,InterstitialActivity.class );
+        finish();
+        startActivity(goToInterstitial);
     }
 }
